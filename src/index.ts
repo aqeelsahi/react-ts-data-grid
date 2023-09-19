5b589ca80e1ccedbb5ff5153f7fc0e82b01c7220
Aqeel <aqeel@agunit.com>
2023-09-19 14:29:03 +1000

    fix: import export table

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export * from './components';
+import table from './components/table/table';
+
+export const Table = table;
 
 export const myPackage = (taco = ''): string => taco + 'from my package x';
 
