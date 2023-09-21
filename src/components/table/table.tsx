b42971d7eb4cc7b3544cd54654d3df3135822f11
Aqeel <aqeel@agunit.com>
2023-09-21 14:20:23 +1000

    fix: table input type

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Table = (data: string[][]): any => {
+const Table = ({ data }: { data: string[][] }): JSX.Element => {
   return (
     <div>
-      {data.map(row => {
-        return <div>{row}</div>;
-      })}
+      {data.map((row, rowIndex) => (
+        <div key={rowIndex} className="table-row">
+          {row.map((cell, cellIndex) => (
+            <div key={cellIndex} className="table-cell">
+              {cell}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,3 @@ import table from './components/table/table';
 export const Table = table;
 
 export const myPackage = (taco = ''): string => taco + 'from my package x';
-
-export const generateCSV = (data: string[][], fileName: string): void => {
-  if (data.length === 0) {
-    console.error('Data array is empty.');
-    return;
-  }
-
-  try {
-    // Create a Blob containing the CSV data
-    const blob = new Blob([data.join('\n')], {
-      type: 'text/csv;charset=utf-8',
-    });
-
-    // Create a temporary URL for the Blob
-    const url = window.URL.createObjectURL(blob);
-
-    // Create a temporary <a> element to trigger the download
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = fileName;
-
-    // Programmatically click the link to trigger the download
-    link.click();
-
-    // Clean up by revoking the URL
-    window.URL.revokeObjectURL(url);
-  } catch (error) {
-    console.error('Error generating CSV:', error);
-  }
-};
