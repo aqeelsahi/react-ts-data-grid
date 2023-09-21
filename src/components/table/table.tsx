a0b3962adad0409386eebe464ca8fad0049c9f42
Aqeel <aqeel@agunit.com>
2023-09-21 14:28:35 +1000

    fix: style init

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,18 +1,52 @@
+// import React from 'react';
+
+// const Table = ({ data }: { data: string[][] }): JSX.Element => {
+//   return (
+//     <div>
+//       {data.map((row, rowIndex) => (
+//         <div key={rowIndex} className="table-row">
+//           {row.map((cell, cellIndex) => (
+//             <div key={cellIndex} className="table-cell">
+//               {cell}
+//             </div>
+//           ))}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Table;
 import React from 'react';
+import './style.css';
 
 const Table = ({ data }: { data: string[][] }): JSX.Element => {
+  if (data.length === 0) {
+    return <div>No data available.</div>;
+  }
+
+  const headerRow = data[0];
+  const rows = data.slice(1);
+
   return (
-    <div>
-      {data.map((row, rowIndex) => (
-        <div key={rowIndex} className="table-row">
-          {row.map((cell, cellIndex) => (
-            <div key={cellIndex} className="table-cell">
-              {cell}
-            </div>
+    <table className="custom-table">
+      <thead>
+        <tr>
+          {headerRow.map((header, index) => (
+            <th key={index}>{header}</th>
           ))}
-        </div>
-      ))}
-    </div>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, cellIndex) => (
+              <td key={cellIndex}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
